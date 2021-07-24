3c435e08a6365a8696aa37ad6aaeb159479682ac
Colten Glover <coltenglover@yahoo.com>
2021-07-23 21:40:00 -0400

    Testing Scenebuiler

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,9 +11,14 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.paint.*;
 import java.awt.*;
+import javafx.scene.*;
+import javafx.scene.text.Font;
+import javafx.scene.shape.*;
+import javafx.scene.image.*;
 
 public class Main extends Application {
 
@@ -24,12 +31,40 @@ public class Main extends Application {
 
     public void start(Stage stage) throws Exception {
 
+        Parent root1 = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setScene(new Scene(root1));
+        stage.show();
+
         //Stage is like a JFrame
 
         Group root = new Group(); // Groups are like layout managers
-        Scene scene = new Scene(root, Color.BLUE); //Scene is like a JPanel
+        Scene scene = new Scene(root, 600, 600, Color.SKYBLUE); //Scene is like a JPanel, can add color as second param
         stage.setTitle("Welcome");
 
+        /*
+
+        //SCENES + DRAWING//
+        Text text = new Text(); //needs to be added to root node
+        text.setText("Wow");
+        text.setX(50); //sets x-coord
+        text.setY(50); //sets y-coord
+        text.setFont(Font.font("Verdana", 50));
+        text.setFill(Color.GREY); //Colors the text
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(300);
+        line.setStrokeWidth(65); // Makes line thicker
+        line.setStroke(Color.CRIMSON);
+        root.getChildren().add(text); // adds the text node
+        root.getChildren().add(line);
+        //SCENES + DRAWING//
+
+         */
+
+
+        /*
         //Image icon = new Image("icon.png"); Looks for image in src
         //stage.getIcons().add(icon); //Sets the top left image in window to new icon
         //stage.setWidth/Height(420); Sets dimensions of window
@@ -37,8 +72,12 @@ public class Main extends Application {
         stage.setFullScreenExitHint("Press q to escape"); //Shows message of how to exit fullscreen mode
         stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
 
-        stage.setScene(scene);
+         */
 
+        /*
+        stage.setScene(scene);
         stage.show();
+
+         */
     }
 }
