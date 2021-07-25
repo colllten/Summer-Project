f1840c72ef9118f485f31a5ce297fd0d1a1eac4e
Colten Glover <coltenglover@yahoo.com>
2021-07-25 16:51:49 -0400

    Method back to login

---
+++ ExpensesPageController.java
@@ -0,0 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ExpensesPageController {
+
+
+
+    private Stage stage;
+    private Scene scene;
+    public void switchToRegisterPage(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("RegisterPage.fxml"));
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
